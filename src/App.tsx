a90c706fd872b4b4756c65215972fb53eefb95b8
AvaN0x <clement.ricatte57@gmail.com>
2021-01-16 11:00:40 +0100

    added route to projects

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,26 @@ import './App.css';
 import NotFound from "./views/Errors/NotFound";
 import Home from './views/Home';
 
+const Redirect = (link: string) => {
+    window.location.href = link;
+    return null;
+}
+
 function App() {
     return (
         <Router>
             <Switch>
                 <Route exact path='/' component={Home} />
+
+
+                {/* GitHub Pages */}
+                <Route exact path="/IUT-ProjetWeb" component={() => Redirect('https://avan0x.github.io/IUT-ProjetWeb/')} />
+                <Route exact path="/AnciensEtudiantsIUTMetz" component={() => Redirect('https://oxypomme.github.io/AnciensEtudiantsIUTMetz/')} />
+
+                {/* Other pages */}
+                <Route exact path="/Matchable" component={() => Redirect('https://matchable-80a41.web.app/')} />
+
+                {/* Error pages */}
                 <Route component={NotFound} />
             </Switch>
         </Router>
