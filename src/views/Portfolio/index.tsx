834835e117ac9f4a6d1a49cf06a5a358f0fd9dbb
AvaN0x <clement.ricatte57@gmail.com>
2021-01-21 16:04:11 +0100

    fix links

--- src/views/Portfolio/index.tsx
+++ src/views/Portfolio/index.tsx
@@ -11,9 +11,9 @@ const Portfolio = () => {
     return (
         <>
             <NavBar>
-                <NavLink to="/#about">About</NavLink> {/* // TODO not working */}
-                <NavLink to="/#portfolio">Portfolio</NavLink> {/* // TODO not working */}
-                <NavLink to="/#contact">Contact</NavLink> {/* // TODO not working */}
+                <a href="#about">About</a>
+                <a href="#portfolio">Portfolio</a>
+                <a href="#contact">Contact</a>
             </NavBar>
 
             <Cover url={"https://wallpaperaccess.com/full/521099.jpg"} >
@@ -21,13 +21,13 @@ const Portfolio = () => {
                 <h1>Clément RICATTE</h1>
             </Cover>
 
-            <ColoredSection title="A propos" id="#about" />
+            <ColoredSection title="A propos" id="about" />
             <About />
 
-            <ColoredSection title="Portfolio" id="#portfolio" />
+            <ColoredSection title="Portfolio" id="portfolio" />
 
 
-            <ColoredSection title="Contact" id="#contact" />
+            <ColoredSection title="Contact" id="contact" />
 
         </>
     );
