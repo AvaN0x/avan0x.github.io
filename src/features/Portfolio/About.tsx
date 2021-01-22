8c86de126aa5a5e3d393d5376d7292e2ef2e6457
AvaN0x <clement.ricatte57@gmail.com>
2021-01-22 09:59:21 +0100

    created an interface for all the about category

--- src/features/Portfolio/About.tsx
+++ src/features/Portfolio/About.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { isMobile } from 'react-device-detect';
 
+import { FirebaseDatabaseNode } from '@react-firebase/database';
 import { Section } from '../../components/styledComponents';
-import Experiences from './About/Experiences/Experiences';
+
+import IAbout from './About/IAbout';
 import Personal from './About/Personal/Personal';
+import Experiences from './About/Experiences/Experiences';
 import Educations from './About/Educations/Educations';
 
-const AboutContainer = styled.div`
+const AboutContainerStyle = styled.div`
     display: flex;
     flex-wrap: wrap;
 `;
@@ -16,14 +19,27 @@ export const AboutItem = styled.div`
     width: clamp(300px, 300px, 100%);
 `;
 
+const AboutContainer = ({ about }: { about: IAbout }) => {
+    return (
+        <AboutContainerStyle>
+            <Personal personal={about.personal} />
+            {/* <Educations /> */}
+            {/* <Experiences /> */}
+        </AboutContainerStyle>
+    );
+}
+
 const About = () => {
     return (
         <Section>
-            <AboutContainer>
-                <Personal />
-                <Educations />
-                <Experiences />
-            </AboutContainer>
+            <FirebaseDatabaseNode
+                path="/about/"
+                orderByKey
+            >
+                {data => !data.isLoading && data.value &&
+                    <AboutContainer about={data.value} />
+                }
+            </FirebaseDatabaseNode>
         </Section>
     );
 }
---
+++ src/features/Portfolio/About/IAbout.tsx
@@ -0,0 +1,7 @@
+import IPersonal from './Personal/IPersonal';
+
+interface IAbout {
+    personal: IPersonal
+}
+
+export default IAbout;
