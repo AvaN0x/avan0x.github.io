9e9d2ab11535f5f0d90dd690b59da25a29bc255c
AvaN0x <clement.ricatte57@gmail.com>
2021-01-25 18:28:14 +0100

    add some links for languages

--- src/components/LanguageComponent.tsx
+++ src/components/LanguageComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 const LanguageImg = styled.img`
     height: 1rem;
@@ -10,40 +11,49 @@ const LanguageImg = styled.img`
     background-color: #00000060;
     position: relative;
     top: -2px;
+    transition: background-color .5s;
+`;
+
+const LanguageLink = styled.a<{ href?: string }>`
+    ${props => props.href && css`
+        &>img:hover {
+            background-color: #ffffff20;
+        }
+    `}
 `;
 
 interface IIcon {
     label: string;
     title?: string;
-    icons?: string[];
+    icons?: { src: string; href?: string }[];
 }
 
 //! source : https://worldvectorlogo.com/fr
 const icons: { [id: string]: IIcon; } = {
-    "windows": { label: "Windows", icons: ["https://cdn.worldvectorlogo.com/logos/microsoft-windows-22.svg"] },
-    "linux": { label: "Linux", icons: ["https://cdn.worldvectorlogo.com/logos/linux-tux.svg", "https://cdn.worldvectorlogo.com/logos/ubuntu-4.svg"] },
-    "office": { label: "Suite office", icons: ["https://cdn.worldvectorlogo.com/logos/office-1.svg"] },
-    "photoshop": { label: "Photoshop", icons: ["https://cdn.worldvectorlogo.com/logos/photoshop-cc-7.svg"] },
-    "git": { label: "Git", icons: ["https://cdn.worldvectorlogo.com/logos/git-icon.svg"] },
+    "windows": { label: "Windows", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/microsoft-windows-22.svg", href: "https://www.microsoft.com/windows/" }] },
+    "linux": { label: "Linux", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/linux-tux.svg" }, { src: "https://cdn.worldvectorlogo.com/logos/ubuntu-4.svg", href: "https://ubuntu.com/" }] },
+    "office": { label: "Suite office", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/office-1.svg", href: "https://www.office.com/" }] },
+    "photoshop": { label: "Photoshop", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/photoshop-cc-7.svg", href: "https://www.adobe.com/fr/products/photoshop.html" }] },
+    "git": { label: "Git", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/git-icon.svg", href: "https://git-scm.com/" }] },
     "mvc": { label: "MVC", title: "Model View Controller" },
-    "asm": { label: "x86 ASM", icons: ["https://www.flaticon.com/svg/static/icons/svg/987/987983.svg"] },
-    "c": { label: "C", icons: ["https://cdn.worldvectorlogo.com/logos/c-2975.svg"] },
-    "cs": { label: "C#", icons: ["https://cdn.worldvectorlogo.com/logos/c--4.svg"] },
+    "asm": { label: "x86 ASM", icons: [{ src: "https://www.flaticon.com/svg/static/icons/svg/987/987983.svg" }] },
+    "c": { label: "C", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/c-2975.svg" }] },
+    "cs": { label: "C#", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/c--4.svg", href: "https://docs.microsoft.com/dotnet/csharp/" }] },
     "wpf": { label: "WPF" },
-    "java": { label: "Java", icons: ["https://cdn.worldvectorlogo.com/logos/java-14.svg"] },
+    "java": { label: "Java", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/java-14.svg" }] },
     "javafx": { label: "JavaFX" },
     "junit": { label: "JUnit" },
-    "lua": { label: "Lua", icons: ["https://cdn.worldvectorlogo.com/logos/lua-5.svg"] },
-    "php": { label: "PHP", icons: ["https://cdn.worldvectorlogo.com/logos/php-1.svg"] },
-    "mysql": { label: "MySQL", icons: ["https://cdn.worldvectorlogo.com/logos/mysql-6.svg"] },
-    "js": { label: "JavaScript", icons: ["https://cdn.worldvectorlogo.com/logos/javascript.svg"] },
-    "ts": { label: "TypeScript", icons: ["https://cdn.worldvectorlogo.com/logos/typescript.svg"] },
-    "react": { label: "React", icons: ["https://cdn.worldvectorlogo.com/logos/react.svg"] },
-    "redux": { label: "Redux", icons: ["https://cdn.worldvectorlogo.com/logos/redux.svg"] },
-    "saga": { label: "Redux-Saga", icons: ["https://cdn.worldvectorlogo.com/logos/redux-saga.svg"] },
-    "ajax": { label: "Ajax", icons: ["https://upload.wikimedia.org/wikipedia/commons/a/a1/AJAX_logo_by_gengns.svg"] },
-    "html": { label: "HTML5", icons: ["https://cdn.worldvectorlogo.com/logos/html-5.svg"] },
-    "css": { label: "CSS3", icons: ["https://cdn.worldvectorlogo.com/logos/css3.svg"] },
+    "lua": { label: "Lua", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/lua-5.svg", href: "http://www.lua.org/" }] },
+    "php": { label: "PHP", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/php-1.svg", href: "https://www.php.net/" }] },
+    "mysql": { label: "MySQL", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/mysql-6.svg", href: "https://www.mysql.com/" }] },
+    "js": { label: "JavaScript", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/javascript.svg" }] },
+    "ts": { label: "TypeScript", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/typescript.svg" }] },
+    "react": { label: "React", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/react.svg", href: "https://reactjs.org/" }] },
+    "redux": { label: "Redux", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/redux.svg", href: "https://redux.js.org/" }] },
+    "saga": { label: "Redux-Saga", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/redux-saga.svg", href: "https://redux-saga.js.org/" }] },
+    "ajax": { label: "Ajax", icons: [{ src: "https://upload.wikimedia.org/wikipedia/commons/a/a1/AJAX_logo_by_gengns.svg" }] },
+    "html": { label: "HTML5", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/html-5.svg" }] },
+    "css": { label: "CSS3", icons: [{ src: "https://cdn.worldvectorlogo.com/logos/css3.svg" }] },
 }
 
 const LanguageComponent = ({ name }: { name: string }): JSX.Element => {
@@ -51,7 +61,9 @@ const LanguageComponent = ({ name }: { name: string }): JSX.Element => {
         return (
             <span title={icons[name].title || ""}>
                 {icons[name].icons?.map((icon, techIndex) => (
-                    <LanguageImg src={icon} key={techIndex} />
+                    <LanguageLink key={techIndex} target="_blank" rel="noreferrer" href={icon.href}>
+                        <LanguageImg src={icon.src} />
+                    </LanguageLink>
                 ))}
 
                 {icons[name].label}
