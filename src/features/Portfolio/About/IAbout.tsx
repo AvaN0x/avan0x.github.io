580e3a96a08bd4622a5b3097dee4e099dd04dc3d
AvaN0x <clement.ricatte57@gmail.com>
2021-02-03 20:07:30 +0100

    change type of qualities

--- src/features/Portfolio/About/IAbout.tsx
+++ src/features/Portfolio/About/IAbout.tsx
@@ -1,3 +1,4 @@
+import ILang from '../../../components/Lang/ILang';
 import IEducation from './Educations/IEducation';
 import IExperience from './Experiences/IExperience';
 import ILanguage from './Languages/ILanguage';
@@ -7,7 +8,7 @@ import ITechs from './Techs/ITechs';
 interface IAbout {
     personal: IPersonal;
     languages: ILanguage[];
-    qualities: string[];
+    qualities: (ILang | string)[];
     educations: IEducation[];
     experiences: IExperience[];
     techs: ITechs;
